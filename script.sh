dd33613d3f47593d75f14f3811df4469896ad84d
eshiant <anthony.shivakumar@antsand.com>
2019-01-19 23:56:05 -0800

    update styles

---
+++ script.sh
@@ -0,0 +1,12 @@
+npm install gulp --no-bin-links
+npm install gulp-browserify --no-bin-links
+npm install gulp-compass --no-bin-links
+npm install gulp-bower-normalize --no-bin-links
+npm install gulp-connect --no-bin-links
+npm install gulp-if --no-bin-links
+npm install gulp-uglify --no-bin-links
+npm install gulp-minify-html --no-bin-links
+npm install gulp-concat --no-bin-links
+npm install gulp-notify --no-bin-links
+npm install gulp-bower --no-bin-links
+
